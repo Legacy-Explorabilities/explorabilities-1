84dc48f80333a01cf9e42c83ede5921d25579ae9
hanyen <me@hanyen.com>
2017-02-08 17:58:33 -0800

    Cleanup Nav.jsx

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -1,12 +1,6 @@
 import React from 'react';
 import {Link} from 'react-router';
-import Auth from '../auth/auth.jsx'; //import Auth to find out if user is logged in ?
 import authHelpers from '../auth/auth-helpers.js';
-
-
-//when user logged out, change the logout text to login
-//need to send user login status down as props?
-//need to make this stateful class
 
 function checkAuth() {
   if (!localStorage.token) {
@@ -34,39 +28,4 @@ export default class Nav extends React.Component {
       </div>
     );
   }
-} 
-
-
-// var text = function() {
-//   //If user is logged in, display 'Log Out'
-//     //else, display 'Log In'
-//   return 'Log Out';
-// }
-
-//verifies the user's token serverside.
-//check out line 60 in server/db/users/usersController.js
-/*function requireAuth(nextState, replace, blah) {
-  axios.get('/users/auth', {
-    headers: { token: localStorage.token || null }
-  })
-  .then((res) => {
-    //res.data.user = user email
-    //res.data.id = user id
-    console.log('User is logged in');
-    console.log('Response is: ', res);
-    this.state.buttonText = 'Log Out'; //change the buttonText state to Log Out
-    console.log(this.state.buttonText);
-  })
-  .catch((err) => {
-    console.log('User is NOT logged in');
-    this.state.buttonText = 'Log In'; //change the buttonText state to Log In
-    replace({
-      pathname: '/auth/signin',
-      state: {
-        nextPathName: nextState.location.pathname
-      }
-    });
-  });
-}*/
-
-// export default Nav;
+}
