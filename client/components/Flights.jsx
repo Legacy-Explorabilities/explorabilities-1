a6f4148dbb6f3f09f833d34265fa92a964fef311
dsvorc41 <danilosvorcan@live.com>
2017-02-09 17:52:17 -0800

    Implement Google QPX API for flight search
    When user clicks on departure airport (within 100 miles of their current geolocation) and arrival airport (within 100 miles of their destination/target location), make API call to Google QPX to get top 3 flights with the two airports.

--- client/components/Flights.jsx
+++ client/components/Flights.jsx
@@ -9,26 +9,50 @@ export default class Flights extends React.Component {
     super(props);
     this.state = {
       departureAirports: [],
-      arrivalAirports: []
+      arrivalAirports: [],
+      userSelectedDepartureAirport: '',
+      userSelectedArrivalAirport: '',
     }
     //setInterval(function(){console.log('hello', props)}, 1000)
   }
 
 
   render() {
-    var departureAirportsView = this.state.departureAirports.map(function(airport) {
+    const context = this;
+    //listen for user clicks on departure airports
+    function clickDepartureAirport(e, airport){
+      e.preventDefault();
+      context.setState({
+        userSelectedDepartureAirport: airport
+      }, ()=>{
+      context.findFlights(context.state.userSelectedDepartureAirport, context.state.userSelectedArrivalAirport);
+      });
+      //call find flights function (it will not trigger unless both arrival and departure
+      //have been selected)
+      /*context.findFlights*/
+    }
+
+    function clickArrivalAirport(e, airport){
+      e.preventDefault();
+      context.setState({
+        userSelectedArrivalAirport: airport
+      }, () => {
+        context.findFlights(context.state.userSelectedDepartureAirport, context.state.userSelectedArrivalAirport);
+      });
+    }
+    let departureAirportsView = this.state.departureAirports.map(function(airport) {
       return (
-        <li key={airport.code}>
-          <span>{airport.code}: </span>
+        <li value={airport.code} key={airport.code} onClick={(e)=>{clickDepartureAirport(e, airport.code)}}>
+          <a href="#">{airport.code}: </a>
           <span>{airport.name}</span>
         </li>
       )
     });
 
-    var arrivalAirportsView = this.state.arrivalAirports.map(function(airport) {
+    let arrivalAirportsView = this.state.arrivalAirports.map(function(airport) {
       return (
-        <li key={airport.code}>
-          <span>{airport.code}: </span>
+        <li key={airport.code} onClick={(e)=>{clickArrivalAirport(e, airport.code)}}>
+          <a href="#">{airport.code}: </a>
           <span>{airport.name}</span>
         </li>
       )
@@ -43,7 +67,7 @@ export default class Flights extends React.Component {
   }
   //listen for updates in props (e.g. finding user's location)
   componentWillReceiveProps(location) {
-    console.log('Location from Flights.jsx ', location)
+    console.log('componentWillReceiveProps ', location)
     if (location.currentUserLocation && location.searchTargetLocation) {
       //only after both curent location and target (vacation/trip) location are found
       //invoke findDepartureAirports and ArrivalAirports
@@ -55,55 +79,57 @@ export default class Flights extends React.Component {
   // }
 
   findFlights(origin, destination) {
-    console.log('huh lets see', this.props);
-    var today = new Date();
-    var dd = today.getDate();
-    //The value returned by getMonth is an integer between 0 and 11, referring 0 to January, 1 to February, and so on.
-    var mm = today.getMonth() + 2; 
-    var yyyy = today.getFullYear();
-    var fullDate = yyyy + '-' + mm + '-' + dd;
-    //console.log(fullDate);
+    if (origin && destination) {   
+      console.log('origin and destination present in findFlights', origin, destination);
+      var today = new Date();
+      var dd = today.getDate();
+      //The value returned by getMonth is an integer between 0 and 11, referring 0 to January, 1 to February, and so on.
+      var mm = today.getMonth() + 2; 
+      var yyyy = today.getFullYear();
+      var fullDate = yyyy + '-' + mm + '-' + dd;
+      //console.log(fullDate);
 
-    //TODO: Create an input field for customer to choose the date
+      //TODO: Create an input field for customer to choose the date
 
-    var flightSearchOptions = {
-      "request": {
-        "slice": [
-          {
-            "origin": "SFO",
-            "destination": "LAX",
-            "date": "2017-03-29"
-          }
-        ],
-        "passengers": {
-          "adultCount": 1,
-          "infantInLapCount": 0,
-          "infantInSeatCount": 0,
-          "childCount": 0,
-          "seniorCount": 0
-        },
-        "solutions": 3,
-        "refundable": false
+      var flightSearchOptions = {
+        "request": {
+          "slice": [
+            {
+              "origin": origin,
+              "destination": destination,
+              "date": "2017-03-29"
+            }
+          ],
+          "passengers": {
+            "adultCount": 1,
+            "infantInLapCount": 0,
+            "infantInSeatCount": 0,
+            "childCount": 0,
+            "seniorCount": 0
+          },
+          "solutions": 3,
+          "refundable": false
+        }
       }
-    };
     
-    var params = JSON.stringify(flightSearchOptions);
-    //console.log(params);
+      var params = JSON.stringify(flightSearchOptions);
+      //console.log(params);
 
-    axios({
-      method: 'post',
-      url: 'https://www.googleapis.com/qpxExpress/v1/trips/search?key=AIzaSyDAneVe-LTFEqyCEcq2FwgIoXzYalmi3is',
-      data: params,
-      headers: {
-        'content-type': 'application/json'
-      }
-    })
-    .then(function(response) {
-      console.log('Success QPX', response);
-    })
-    .catch(function(error) {
-      console.log('ERROR QPX!', error);
-    }); 
+      axios({
+        method: 'post',
+        url: 'https://www.googleapis.com/qpxExpress/v1/trips/search?key=AIzaSyDAneVe-LTFEqyCEcq2FwgIoXzYalmi3is',
+        data: params,
+        headers: {
+          'content-type': 'application/json'
+        }
+      })
+      .then(function(response) {
+        console.log('Success QPX', response);
+      })
+      .catch(function(error) {
+        console.log('ERROR QPX!', error);
+      }); 
+    }
   }
   //make a post request to the server, so server can make get request for IATA CODES (Circumvent CORS)
   findDepartureAirports(currentUserLocation) {
