5a1e1d664810f4a021cd63fb1687540226341927
Anukul <shenoyanukul@gmail.com>
2017-02-11 15:10:37 -0800

    rendering dummy deal data

--- client/components/Deals.jsx
+++ client/components/Deals.jsx
@@ -1,15 +1,38 @@
 import React from 'react';
 import axios from 'axios';
 
+
 export default class Deals extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+        data: {
+            contents: [
+            {
+                productID: 34,
+                productName: "SuperWidget",
+                quantity: 1
+            },
+            {
+                productID: 56,
+                productName: "WonderWidget",
+                quantity: 3
+            }
+        ]
         }
+        }
+    }
+    componentDidMount() {
+        console.log(this.state.data);
     }
     render() {
         return (
-        <h2>DEALS</h2>
+            <div>
+                <h2>DEALS</h2>
+                {this.state.data.contents.map(function(obj){
+                    return (<h3>{obj.productName}</h3>);
+                })}
+            </div>
         );
     }
 }
